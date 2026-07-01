324fe24b9f291390d7c5fc6b16c794f5fbc0ed1f
nawnawnawnawnaw55 <nawnawnawnawnaw55@gmail.com>
2026-07-01 02:09:33 -0500

    Update

--- api/fetch-reviews-cache.js
+++ api/fetch-reviews-cache.js
@@ -55,7 +55,7 @@ function fetchGoogleReviews() {
 }
 
 function sanitizeText(t) {
-  return (t || '').replace(/[â-â¿¿]/g, '').replace(/\s+/g, ' ').trim();
+  return (t || '').replace(/[^\x20-\x7E\xA0-\xFF\u2010-\u2122]/g, '').replace(/\s+/g, ' ').trim();
 }
 
 async function main() {
